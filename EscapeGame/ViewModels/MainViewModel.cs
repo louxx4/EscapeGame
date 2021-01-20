ca02ac729d61f6cfa9ce4d3d9249792a734601ab
Lule <amey0110@gmail.com>
2021-01-20 18:09:22 +0100

    Init

---
+++ EscapeGame/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EscapeGame.Models;
+
+namespace EscapeGame.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void notifyOnPropertyChanged(string propName) {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
